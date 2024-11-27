7e781ec53ee079b806abe30551184b7d6db828d8
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-11-27 00:36:16 -0800

    Finished working on week08 labs

--- src/week08/Hotel.java
+++ src/week08/Hotel.java
@@ -15,16 +15,37 @@ public class Hotel {
         System.out.println("How many people are in your party");
         int numberOfPeople = input.nextInt();
 
+        input.close();
+
         int price = 0;
-        String roomType = "";
+        String roomType = "";// In order to utilize these two variables adjust the code below to
 
         switch (numberOfPeople) {
-            case 1 -> System.out.println(roomType = "| single room |" + " price: $" + numberOfDayStaying * 100);
-            case 2 -> System.out.println(roomType = "| double room |" + " price: $" + numberOfDayStaying * 125);
-            case 3, 4 -> System.out.println(roomType = "| large room |" + " price: $" + numberOfDayStaying * numberOfPeople * 50);
-            case 5, 6 -> System.out.println(roomType = "| suite |" + " price: $" + numberOfDayStaying * 1000);
-            default -> System.err.println("Sorry " + numberOfPeople + " is not a valid group size");
+            case 1 -> {
+                roomType = "| single room |";
+                price = numberOfDayStaying * 100;
+            }
+            case 2 -> {
+                roomType = "| double room |";
+                price = numberOfDayStaying * 125;
+            }
+            case 3, 4 -> {
+                roomType = "| large room |";
+                price = numberOfDayStaying * numberOfPeople * 50;
+            }
+            case 5, 6 -> {
+                roomType = "| suite |";
+                price = numberOfDayStaying * 1000;
+
+            }
+            default -> {
+                System.err.println("Sorry " + numberOfPeople + " is not a valid group size");
+                return;
+            }
+
         }
+
+        System.out.println("people " + numberOfPeople + " " + roomType + " price:" + price);
 
     }
 
