5dc6ffac5fccfb9999031d128c0c812c7372fa0e
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-11-27 00:20:24 -0800

    Working on week08 labs

--- src/week08/Hotel.java
+++ src/week08/Hotel.java
@@ -1,4 +1,31 @@
 package week08;
 
+import java.util.Scanner;
+
 public class Hotel {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to the Java Hotel!");
+
+        System.out.println("How many days will you stay?");
+        int numberOfDayStaying = input.nextInt();
+
+        System.out.println("How many people are in your party");
+        int numberOfPeople = input.nextInt();
+
+        int price = 0;
+        String roomType = "";
+
+        switch (numberOfPeople) {
+            case 1 -> System.out.println(roomType = "| single room |" + " price: $" + numberOfDayStaying * 100);
+            case 2 -> System.out.println(roomType = "| double room |" + " price: $" + numberOfDayStaying * 125);
+            case 3, 4 -> System.out.println(roomType = "| large room |" + " price: $" + numberOfDayStaying * numberOfPeople * 50);
+            case 5, 6 -> System.out.println(roomType = "| suite |" + " price: $" + numberOfDayStaying * 1000);
+            default -> System.err.println("Sorry " + numberOfPeople + " is not a valid group size");
+        }
+
+    }
+
 }
